605a3f75b3836e315cc43a32fe75454fd57ffa57
Odai Atef <o.3odai@gmail.com>
2022-10-07 16:48:21 +0300

    index using elastic

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function postReq($data){
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,"http://3.125.9.240:9221/tweets/_doc");
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLINFO_CONTENT_TYPE, 1);
+    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+
+    curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $result = curl_exec($ch);
+    echo "<pre>$result</pre>";
+    curl_close ($ch);
+}
+$dir=__DIR__."/vectors";
+$docs=scandir($dir);
+foreach ($docs as $doc){
+    if(!is_dir($dir."/$doc")){
+        $data=file_get_contents($dir."/$doc");
+        postReq($data);
+
+    }
+}
+postReq([]);
