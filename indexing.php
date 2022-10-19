0b33cdd0c6ee9fc90554d5e3e6c85090aa4ea86e
Odai Atef <o.3odai@gmail.com>
2022-10-19 18:12:51 +0300

    fixing vectors

--- indexing.php
+++ indexing.php
@@ -3,10 +3,10 @@
 
 function postReq($data){
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL,"http://3.125.9.240:9221/tweets/_doc");
+    curl_setopt($ch, CURLOPT_URL,"http://3.125.9.240:9221/products/_doc");
     curl_setopt($ch, CURLOPT_POST, 1);
     curl_setopt($ch, CURLINFO_CONTENT_TYPE, 1);
-    curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
+    curl_setop t( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
     curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
@@ -14,7 +14,7 @@ function postReq($data){
     echo "<pre>$result</pre>";
     curl_close ($ch);
 }
-$dir=__DIR__."/vectors";
+$dir=__DIR__."/var/www/html/vectors";
 $docs=scandir($dir);
 foreach ($docs as $doc){
     if(!is_dir($dir."/$doc")){
