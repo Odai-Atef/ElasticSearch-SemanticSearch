5f7f4cace081caa13537e28feb7c146b0495eea7
Odai Atef <o.3odai@gmail.com>
2022-10-24 20:46:37 +0300

    __DIR__

--- indexing.php
+++ indexing.php
@@ -5,7 +5,7 @@ function postReq($data){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL,"http://3.125.9.240:9221/products/_doc");
     curl_setopt($ch, CURLOPT_POST, 1);
-    curl_setopt($ch, CURLINFO_CONTENT_TYPE, 1);
+    // curl_setopt($ch, CURLINFO_CONTENT_TYPE, 1);
     curl_setopt( $ch, CURLOPT_HTTPHEADER, array('Content-Type:application/json'));
 
     curl_setopt($ch, CURLOPT_POSTFIELDS,$data);
