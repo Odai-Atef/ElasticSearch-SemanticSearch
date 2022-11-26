1f9439bb94d8abe7b72bb0396d0b3ec26e6e8c50
Odai Atef <o.3odai@gmail.com>
2022-11-26 12:26:53 +0300

    fixing stop words with aggs

--- stop_words/app/Http/Controllers/SearchController.php
+++ stop_words/app/Http/Controllers/SearchController.php
@@ -21,12 +21,12 @@ class SearchController extends Controller
                             "field" => "description.text.keyword"
                         ]
                     ],
-                            "my-suggestion-title" => [
-                                "text" => "$keyword",
-                                "term" => [
-                                    "field" => "name.text.keyword"
-                                ]
-                            ]
+                    "my-suggestion-title" => [
+                        "text" => "$keyword",
+                        "term" => [
+                            "field" => "name.text.keyword"
+                        ]
+                    ]
                 ]
             ]
         );
@@ -44,14 +44,14 @@ class SearchController extends Controller
 
     function mapping($request)
     {
-        $map_text = ["variants_details_value"=>"variants.details.value","categories_name_text_keyword"=>"categories.name.text.keyword","tags", "store_id","tags"];
+        $map_text = ["variants_details_value" => "variants.details.value", "categories_name_text_keyword" => "categories.name.text.keyword", "tags", "store_id", "tags"];
         $map_min = ["min_price", "min_rating"];
         $map_max = ["max_price", "max_rating"];
-        foreach ($map_text as $key=> $keyword) {
-            if(is_numeric($key)){
-                $map_key=$keyword;
-            }else{
-                $map_key=$key;
+        foreach ($map_text as $key => $keyword) {
+            if (is_numeric($key)) {
+                $map_key = $keyword;
+            } else {
+                $map_key = $key;
             }
             if ($request->query($map_key)) {
                 $this->filter_by_text($keyword, $request->query($map_key));
@@ -59,12 +59,12 @@ class SearchController extends Controller
         }
         foreach ($map_min as $keyword) {
             if ($request->query($keyword)) {
-                $this->filter_by_range(str_replace("min_","",$keyword), $request->query($keyword),"gte");
+                $this->filter_by_range(str_replace("min_", "", $keyword), $request->query($keyword), "gte");
             }
         }
         foreach ($map_max as $keyword) {
             if ($request->query($keyword)) {
-                $this->filter_by_range(str_replace("max_","",$keyword), $request->query($keyword),"lte");
+                $this->filter_by_range(str_replace("max_", "", $keyword), $request->query($keyword), "lte");
             }
         }
 
@@ -74,10 +74,12 @@ class SearchController extends Controller
     {
         $this->query["query"]['bool']['must'][1]['match'][$key] = $value;
     }
-    function filter_by_range($key, $value,$op)
+
+    function filter_by_range($key, $value, $op)
     {
         $this->query["query"]['bool']['must'][]['range'][$key][$op] = $value;
     }
+
     function fuzzy_with_category($stop_words, $category_id)
     {
         $query = [
@@ -130,14 +132,14 @@ class SearchController extends Controller
         $stop_words = getStopWords();
         $data['page_title'] = 'Fuzzy Search Products';
         $this->query = [
-            "size" => 100,
+            "size" => 250,
             "query" => [
                 "bool" => [
                     "must" => [
                         [
                             "query_string" => [
                                 "fields" => [
-                                    //  "product_name",
+                                    "name.text",
                                     "description.text"
                                 ],
                                 "query" => trim($_GET['keyword']) . " AND !($stop_words)",
@@ -160,15 +162,15 @@ class SearchController extends Controller
         if ($keyword == "") {
             unset($this->query["query"]["bool"]["must"][0]);
         }
-        $aggs=new AggsController();
-        $this->query["query"]["bool"]["must"]=array_values($this->query["query"]["bool"]["must"]);
+        $aggs = new AggsController();
+        $this->query["query"]["bool"]["must"] = array_values($this->query["query"]["bool"]["must"]);
         $data['data_fuzzy'] = postReq($this->query);
         $data["suggestions"] = $this->suggest($keyword);
-        $data['categories']=$aggs->categories($keyword);
-        $data['variants_data']=$aggs->variants($keyword);
-        $data['variants_value_data']=$aggs->variants_values($keyword);
-        $data['stores']=$aggs->stores($keyword);
-        $data['tags']=$aggs->tags_values($keyword);
+        $data['categories'] = $aggs->categories($keyword);
+        $data['variants_data'] = $aggs->variants($keyword);
+        $data['variants_value_data'] = $aggs->variants_values($keyword);
+        $data['stores'] = $aggs->stores($keyword);
+        $data['tags'] = $aggs->tags_values($keyword);
 
         return view('search', $data);
     }
